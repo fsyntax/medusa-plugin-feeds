cb0cec95b0494e3901882bdd2060ceca92b5396d
Xristos-Marios Dimopoulos <cdimopoulos@andko.gr>
2023-09-13 20:09:43 +0300

    removed medusaprovider since it's not needed

--- src/admin/routes/feed/page.tsx
+++ src/admin/routes/feed/page.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { RouteConfig } from '@medusajs/admin';
-import { MedusaProvider } from 'medusa-react';
-import { QueryClient } from '@tanstack/react-query';
 
 import FeedTable from './components/Table';
 import FeedJsonView from './components/FeedJsonView';
 import FeedXmlView from './components/FeedXmlView';
 
-const queryClient = new QueryClient();
-
 const FeedPage: React.FC = () => {
   return (
-    <MedusaProvider
-      queryClientProviderProps={{ client: queryClient }}
-      baseUrl="http://localhost:9000"
-    >
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5">
-          <FeedXmlView />
-          <FeedJsonView />
-        </div>
-        <FeedTable />
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-5">
+        <FeedXmlView />
+        <FeedJsonView />
       </div>
-    </MedusaProvider>
+      <FeedTable />
+    </div>
   );
 };
 
